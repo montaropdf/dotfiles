2d88acb27be80bd0748d27ca617fa9e3f028eb7a
Roland Everaert <computing.re@revecloud.xyz>
2021-03-02 16:17:17 +0100

    Added "mentioned under --force) will be removed.", added dotfiles/plain/work/.config/emacs/localhost/init-font.el, modified dotfiles/plain/work/.config/emacs/localhost/profile.el, modified filelist

---
+++ dotfiles/plain/work/.config/emacs/localhost/init-font.el
@@ -0,0 +1,2 @@
+(setq default-frame-alist '((font . "SauceCodePro Nerd Font Mono-6")))
+(setq initial-frame-alist '((font . "SauceCodePro Nerd Font Mono-6")))
--- dotfiles/plain/work/.config/emacs/localhost/profile.el
+++ dotfiles/plain/work/.config/emacs/localhost/profile.el
@@ -2,6 +2,7 @@
 			"setup-hydra-menu.el"
 			"init-cl.el"
 			"init-guix.el"
-			"setup-forge.el")))
+			"setup-forge.el"
+			"init-font.el")))
   (dolist (init-file init-file-list)
     (load-if-exists (f-expand init-file emacs-host-cfg-dir))))

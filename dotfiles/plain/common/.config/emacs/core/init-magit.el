e6b47c48f2d35af4f04d994e5e45e4fba6364b27
Roland Everaert <computing.re@revecloud.xyz>
2021-03-02 15:34:43 +0100

    Modified dotfiles/plain/common/.config/emacs/core/init-magit.el, modified dotfiles/plain/work/.config/emacs/localhost/profile.el

--- dotfiles/plain/common/.config/emacs/core/init-magit.el
+++ dotfiles/plain/common/.config/emacs/core/init-magit.el
@@ -1,3 +1,10 @@
 (use-package magit
   :ensure t
   :bind ("C-x g" . magit-status))
+
+(use-package emacsql-sqlite
+  :ensure t)
+
+(use-package forge
+  :ensure t
+  :after (magit emacsql-sqlite))
--- dotfiles/plain/work/.config/emacs/localhost/profile.el
+++ dotfiles/plain/work/.config/emacs/localhost/profile.el
@@ -1,6 +1,7 @@
-(let ((init-file-list '("init-use-proxy.el"
-			"init-hydra.el"
+(let ((init-file-list '("init-proxy-mode.el"
+			"setup-hydra-menu.el"
 			"init-cl.el"
-			"init-guix.el")))
+			"init-guix.el"
+			"setup-forge.el")))
   (dolist (init-file init-file-list)
     (load-if-exists (f-expand init-file emacs-host-cfg-dir))))
